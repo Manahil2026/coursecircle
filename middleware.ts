1093e5c77e1a76e50717a62fde10665caf792041
Manahil2026 <manahil.im111@gmail.com>
2025-02-04 11:10:57 -0500

    add rbac authentication to dashboards

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,31 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(['/pages(.*)'])
+const isAdminRoute = createRouteMatcher(['/pages/admin(.*)'])
+const isProfessorRoute = createRouteMatcher(['/pages/professor(.*)'])
+const isStudentRoute = createRouteMatcher(['/pages/student(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+  if (isProtectedRoute(req)){
+    await auth.protect()
+
+    if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'uni_admin') {
+      const url = new URL('/', req.url)
+      return NextResponse.redirect(url)
+    }
+
+    if (isProfessorRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'prof') {
+      const url = new URL('/', req.url)
+      return NextResponse.redirect(url)
+    }
+
+    if (isStudentRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'member') {
+      const url = new URL('/', req.url)
+      return NextResponse.redirect(url)
+    }
+
+  }
 })
 
 export const config = {
