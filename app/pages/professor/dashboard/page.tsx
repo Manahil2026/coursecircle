2fbffa71170607599e063a066e9914b0e37cc561
Manahil2026 <manahil.im111@gmail.com>
2025-02-04 09:45:27 -0500

    create professor dashboard page

---
+++ app/pages/professor/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProfessorDashboard() {
+  return (
+    <div><p>This is the protected professor dashboard restricted to users with the `professor` role.</p></div>
+  )
+}
