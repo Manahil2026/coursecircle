7b5d29434c3a7fe8298f61448c60fd4946f66d9a
Manahil2026 <manahil.im111@gmail.com>
2025-02-04 09:45:14 -0500

    create admin dashboard page

---
+++ app/pages/admin/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function AdminDashboard() {
+  return (
+    <div><p>This is the protected admin dashboard restricted to users with the `admin` role.</p></div>
+  )
+}
+
