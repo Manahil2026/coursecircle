3b1b1346abc5f146f27f4c7038ed7fe7255486ef
Manahil2026 <manahil.im111@gmail.com>
2025-02-04 10:00:05 -0500

    edit protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/pages/admin/dashboard','/pages/professor/dashboard','/pages/student/dashboard'])
+const isProtectedRoute = createRouteMatcher(['/pages(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
