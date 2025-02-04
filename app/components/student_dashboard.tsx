aa1215862d2423a84f686ecaa442e294c8a5af14
Joseph Boateng <josephboatengcollege45@gmail.com>
2025-02-04 09:23:07 -0500

    Added icons

---
+++ app/components/student_dashboard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Student_dashboard: React.FC = () => {
+  return (
+    <div className="text-gray-800">
+        Student Dashboard
+      
+    </div>
+  );
+};
+
+export default Student_dashboard;
