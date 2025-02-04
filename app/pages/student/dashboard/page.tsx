b43f71e837654b7795de0814d76494d638328fb3
Manahil2026 <manahil.im111@gmail.com>
2025-02-04 09:45:39 -0500

    create student dashboard page

---
+++ app/pages/student/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudentDashboard() {
+  return (
+    <div><p>Registered members will default to student and can access this page.</p></div>
+  )
+}
