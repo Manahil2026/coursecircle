a7f7759ac1d0b358e9d7712dea5fa7e01442d776
Manahil2026 <manahil.im111@gmail.com>
2025-02-05 09:47:45 -0500

    comment out redirect after authetication

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import { redirect } from "next/navigation";
 import Landing_page from "./components/landing_page";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { sessionClaims } = await auth();
+  const role = sessionClaims?.metadata?.role as "uni_admin" | "member" | "prof" | undefined;
+  
+  {/*if (role === "member") {
+    return redirect("/pages/student/dashboard");
+  } else if (role === "prof") {
+    return redirect("/pages/professor/dashboard");
+  } else if (role === "uni_admin") {
+    return redirect("/pages/admin/dashboard");
+  }
+  */} //Still working on it
+
   return (
     <div>
       <Landing_page />
